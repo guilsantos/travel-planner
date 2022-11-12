8d3cf073694341bb83f23817690697dfc2b28069
Guilherme Silva <gsilva@teamworks.com>
2022-11-12 01:12:28 -0300

    Start the Combobox creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,50 @@
 import { useState } from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 import { useQueryCities, useQueryDistance } from "./services";
 
+const ComboBox = ({ title }: any) => {
+  const [value, setValue] = useState<string | null | undefined>();
+  const [city, setCities] = useState("");
+  const queryCities = useQueryCities(city);
+
+  const citiesList = queryCities.data?.map((c) => c[0]);
+
+  return (
+    <Autocomplete
+      onChange={(event: any, newValue: string | null) => {
+        setValue(newValue);
+      }}
+      value={value}
+      onInputChange={(e, newValue) => {
+        console.log(newValue);
+        setCities(newValue as string);
+      }}
+      inputValue={city}
+      disablePortal
+      options={citiesList || []}
+      sx={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label={title} />}
+    />
+  );
+};
+
 const App = () => {
-  const [cities, setCities] = useState("");
+  // const queryDistance = useQueryDistance(["Paris", "Marseille", "Lyon"]);
 
-  const queryCities = useQueryCities(cities);
-  const queryDistance = useQueryDistance(["Paris", "Marseille", "Lyon"]);
-
-  console.log({ queryCities, queryDistance });
+  // console.log({ queryCities, queryDistance });
 
   return (
     <div>
-      <input
-        placeholder="CITY NAME"
-        onChange={(e) => setCities(e.target.value)}
-      />
+      <ComboBox title="City of origin" />
+      <ComboBox title="City of destination" />
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
     </div>
   );
 };
--- src/services/api/cities.ts
+++ src/services/api/cities.ts
@@ -2,6 +2,7 @@ import { CITIES } from "./constants";
 
 export const getCitiesAPI = (citie: string) => {
   if (citie === "fail") throw new Error("API error");
+  if (!citie) return [];
 
   return CITIES.filter((city) =>
     city?.[0]?.toLowerCase().includes(citie.toLowerCase())
